a0fb34df6b0011017ade778a79e097f42b2d95b6
koldin108902 <alex108902@naver.com>
2024-03-18 16:36:33 +0900

    Feat: Jsdocs 타입 추가

---
+++ src/lib/types/Category.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef Category
+ * @property {number} categoryId
+ * @property {string} name
+ * @property {number} sort
+ * @property {string} status
+ * @property {string} clientId
+ */
---
+++ src/lib/types/Service.js
@@ -0,0 +1,16 @@
+/**
+ * @typedef Service
+ * @property {number} serviceId
+ * @property {string} clientId
+ * @property {number} providerId
+ * @property {number} categoryId
+ * @property {number} apiServiceId
+ * @property {string} name
+ * @property {string} type
+ * @property {number} rate
+ * @property {number} min
+ * @property {number} max
+ * @property {string} description
+ * @property {string} status
+ * @property {number} originalRate
+ */
