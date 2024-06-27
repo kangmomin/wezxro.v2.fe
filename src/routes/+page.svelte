02f314d258e96b08356acc3c41761a0e98359f04
kangmomin <alex108902@naver.com>
2024-06-27 19:49:56 +0900

    Feat: 어디비 클라이언트 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Wezxro from "$lib/rendingPage/Wezxro.svelte";
 	import Lotoal from "$lib/rendingPage/Lotoal.svelte";
+	import Adiviy from "$lib/rendingPage/Adiviy.svelte";
 	export let data
 
 </script>
@@ -14,5 +15,8 @@
 {/if}
 {#if data.props.DOMAIN === "lotoal.com"}
 	<Lotoal data="{data}"></Lotoal>
+{/if}
+{#if data.props.DOMAIN === "adiviy.com"}
+	<Adiviy data="{data}"></Adiviy>
 {/if}
 
