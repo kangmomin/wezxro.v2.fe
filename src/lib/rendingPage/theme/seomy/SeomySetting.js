fb9e30bc226f7f0675419cc8ea6d200ce020964a
kangmomin <alex108902@naver.com>
2025-03-10 12:04:47 +0900

    Feat: sns 메이드, Fix: css 전역 적용 부분 이슈 수정

--- src/lib/rendingPage/theme/seomy/SeomySetting.js
+++ src/lib/rendingPage/theme/seomy/SeomySetting.js
@@ -2,12 +2,14 @@ import WEZXRO from './data/WEZXRO.json';
 import WEODUS from './data/WEODUS.json';
 import LOTOAL from './data/LOTOAL.json';
 import ADIVIY from './data/ADIVIY.json';
-import FOLLOUS from './data/FOLLOUS.json'
+import FOLLOUS from './data/FOLLOUS.json';
+import SNSMAID from './data/SNSMAID.json'
 
 export default {
     WEZXRO,
     WEODUS,
     LOTOAL,
     ADIVIY,
-    FOLLOUS
+    FOLLOUS,
+    SNSMAID
 };
